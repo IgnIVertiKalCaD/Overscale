acbb32edcc4a5df31ca02c473b97bbe83dc306e0
IgnI VerTiKalCaD <54883747+IgnIVertiKalCaD@users.noreply.github.com>
2023-04-24 21:40:10 +0300

    poh

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -3,33 +3,31 @@
 
             <div class="inner-header flex ">
                 <transition name="transform-right" appear>
-                    <div class="frame-for-decorate">
-                <v-img
-                        class="logo"
-                        :src="logo">
-                    </v-img>
-                    </div>
+                    <card_outline
+                        :image="logo"
+                        :title="project_name"
+                    />
                 </transition>
-                <transition name="transform-left" appear>
-                    <h1>Overscale</h1>
-                </transition>
+                <section class="flex flex-column ">
+                    <span   class="design-text"
+                        v-for="(item, index) of title_project" :key="index">{{item.text}}</span>
+                </section>
 <!--                <router-link :to="{name: 'admin'}">Go to Home</router-link>-->
-                    <ardialog/>
+<!--
+      <ardialog/>-->
+
             </div>
 
-            <div>
-                <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
-                    <defs>
-                        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-                    </defs>
-                    <g class="parallax">
-                        <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
-                        <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
-                        <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
-                        <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
-                    </g>
-                </svg>
-            </div>
+<!--            <div>-->
+<!--                <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">-->
+<!--                    <defs>-->
+<!--                        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />-->
+<!--                    </defs>-->
+<!--                    <g class="parallax">-->
+<!--                        <use xlink:href="#gentle-wave" v-for="(wave, index) of waves" :key="index" :x="wave.x" :y="wave.y" :fill="wave.color" />-->
+<!--                    </g>-->
+<!--                </svg>-->
+<!--            </div>-->
         </div>
 </template>
 
@@ -37,18 +35,40 @@
 
 import logo from "@/assets/logo.svg"
 import ardialog from "@/views/ar/UI/ARDialog.vue"
+import card_outline from "@/views/ar/UI/card_logo.vue"
+
 
 import { defineComponent } from "vue";
 export default defineComponent({
     name: "MainPage",
     data: () => ({
         logo,
+        project_name: 'Phantom',
+        title_project: [
+            {
+                text: 'downloaded.'
+            },
+            {
+                text: 'installed.'
+            },
+            {
+                text: 'use.'
+            }
+        ],
+        //render waves
+        // waves: [
+        //     {
+        //         x: 48,
+        //         y: 0,
+        //         color: '#1f1f1f'
+        //     }
+        // ]
     }),
     methods: {
     },
     created() {
     },
-    components: { ardialog
+    components: { ardialog, card_outline
     },
     computed: {
     },
@@ -56,6 +76,7 @@ export default defineComponent({
     },
 });
 </script>
+
 
 <style scoped lang="scss">
 .frame-for-decorate {
@@ -83,9 +104,15 @@ export default defineComponent({
     opacity: 0;
 }
 
-
+@import url('https://fonts.cdnfonts.com/css/bruno-ace-sc');
 @import url(//fonts.googleapis.com/css?family=Lato:300:400);
 
+.design-text {
+  font-family: 'Bruno Ace SC', sans-serif;
+  font-size: 3rem;
+  font-weight: bold;
+  text-transform: uppercase;
+}
 
 h1 {
     font-family: 'Lato', sans-serif;
@@ -104,7 +131,8 @@ p {
 .header {
     position:relative;
     text-align:center;
-    background: linear-gradient(60deg, rgb(177, 72, 203) 0%, rgb(125, 88, 225) 100%);
+    background: black;
+    height: 100%;
     color:white;
 }
 .logo {
@@ -113,75 +141,70 @@ p {
 }
 
 .inner-header {
-    height:65vh;
-    width:100%;
-    margin: 0;
-    padding: 0;
+    height:100%;
 }
 
 .flex {
     display: flex;
     text-align: center;
-    flex-direction: column;
+    flex-direction: row;
     align-items: center;
     justify-content: center;
 }
 
-.waves {
-    position:relative;
-    width: 100%;
-    height:15vh;
-    margin-bottom:-7px;
-    min-height:100px;
-    max-height:150px;
-}
-
-.content {
-    position:relative;
-    height:20vh;
-    text-align:center;
-    background-color: white;
-}
-
-
-.parallax > use {
-    animation: move-forever 25s cubic-bezier(.55,.5,.45,.5)     infinite;
-}
-.parallax > use:nth-child(1) {
-    animation-delay: -2s;
-    animation-duration: 7s;
-}
-.parallax > use:nth-child(2) {
-    animation-delay: -3s;
-    animation-duration: 10s;
-}
-.parallax > use:nth-child(3) {
-    animation-delay: -4s;
-    animation-duration: 13s;
-}
-.parallax > use:nth-child(4) {
-    animation-delay: -5s;
-    animation-duration: 20s;
-}
-@keyframes move-forever {
-    0% {
-        transform: translate3d(-90px,0,0);
-    }
-    100% {
-        transform: translate3d(85px,0,0);
-    }
-}
-/*Shrinking for mobile*/
-@media (max-width: 768px) {
-    .waves {
-        height:40px;
-        min-height:40px;
-    }
-    .content {
-        height:30vh;
-    }
-    h1 {
-        font-size:24px;
-    }
-}
+//.waves {
+//    position:relative;
+//    width: 100%;
+//}
+//
+//.content {
+//    position:relative;
+//    height:20vh;
+//    text-align:center;
+//    background-color: white;
+//}
+//.parallax {
+//    max-height: 10px;
+//}
+//
+//.parallax > use {
+//    animation: move-forever 25s cubic-bezier(.55,.5,.45,.5)     infinite;
+//}
+//.parallax > use:nth-child(1) {
+//    animation-delay: -2s;
+//    animation-duration: 20s;
+//}
+//.parallax > use:nth-child(2) {
+//    animation-delay: -3s;
+//    animation-duration: 10s;
+//}
+//.parallax > use:nth-child(3) {
+//    animation-delay: -4s;
+//    animation-duration: 13s;
+//}
+//.parallax > use:nth-child(4) {
+//    animation-delay: -5s;
+//    animation-duration: 20s;
+//}
+//@keyframes move-forever {
+//    0% {
+//        transform: translate3d(-90px,0,0);
+//    }
+//    100% {
+//        transform: translate3d(85px,0,0);
+//    }
+//}
+///*Shrinking for mobile*/
+//@media (max-width: 768px) {
+//    .waves {
+//        height:40px;
+//        min-height:40px;
+//    }
+//    .content {
+//        height:30vh;
+//    }
+//    h1 {
+//        font-size:24px;
+//    }
+//}
 </style>
---
+++ src/views/ar/UI/card_logo.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-card
+            class="ma-0 pa-10 mr-5"
+            variant="outlined"
+    >
+        <v-card-item>
+            <div>
+                <div class="text-overline mb-1">
+                    <v-img :src="image"></v-img>
+                </div>
+                <div class="mb-1 design-text">
+                    {{ title }}
+                </div>
+                <div class="text-h5">CMS made with soul</div>
+            </div>
+        </v-card-item>
+
+        <v-card-actions class="justify-center">
+            <v-btn v-for="(soc, index) in social" :key="index" :href="soc.link" variant="outlined">
+                {{ soc.name }}
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: "card_outlined",
+    props: {
+        title: String,
+        image: String,
+    },
+    data: () => ({
+        social: [
+            {
+                name: 'github',
+                link: 'textlink'
+            },
+            {
+                name: 'discord',
+                link: 'textlink'
+            },
+        ]
+    })
+}
+</script>
+
+<style scoped>
+.design-text {
+    font-family: 'Bruno Ace SC', sans-serif;
+    font-size: 3rem;
+    font-weight: bold;
+    text-transform: uppercase;
+}
+</style>
